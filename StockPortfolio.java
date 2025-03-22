c17b1b4f28e7d0a73de42a739e78935f9db8f537
madhups5 <149224951+madhups5@users.noreply.github.com>
2025-03-22 10:38:12 +0000

    playground challenge 1 completed

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -4,26 +4,36 @@ class StockPortfolio {
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
+    private double highestPrice;
 
-    // TODO: Initialize stock details
-    public StockPortfolio(String symbol, int quantity, double buyingPrice) {
-        // TODO
-    }
 
-    // TODO: Update current market price
-    public void updatePrice(double newPrice) {
-        // TODO
+   // Constructor
+   public StockPortfolio(String symbol, int quantity, double buyingPrice) {
+    if (quantity <= 0 || buyingPrice <= 0) {
+        throw new IllegalArgumentException("Quantity and buying price must be positive.");
     }
+    this.symbol = symbol;
+    this.quantity = quantity;
+    this.buyingPrice = buyingPrice;
+    this.currentPrice = buyingPrice;
+    this.highestPrice = buyingPrice;
+}
 
-    // TODO: Calculate total profit or loss
-    public double calculateProfit() {
-        // TODO
-        return 0.0;
+// Update
+public void updatePrice(double newPrice) {
+    if (newPrice <= 0) {
+        throw new IllegalArgumentException("Price must be positive.");
     }
+    this.currentPrice = newPrice;
+    if (newPrice > this.highestPrice) {
+        this.highestPrice = newPrice;
+    }
+}
+public double calculateProfit() {
+    return (currentPrice - buyingPrice) * quantity;
+}
 
-    // TODO: Calculate current total value of holdings
-    public double getCurrentValue() {
-        // TODO
-        return 0.0;
-    }
+public double getCurrentValue() {
+    return currentPrice * quantity;
+}
 }
